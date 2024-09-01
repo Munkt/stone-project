b8b48a51fb7a8a77a3a68ec1258adc4968eba745
Munkt <110991832+Munkt@users.noreply.github.com>
2024-09-02 02:19:54 +0900

    0901
    pocket 안에 있는 돌들을 원래 위치로 돌려보내는 것 해결 못함

--- js/stone.js
+++ js/stone.js
@@ -1,18 +1,23 @@
 const myStones = document.getElementsByClassName("stone");
-const movePocket = document.getElementById("pocket_container");
-const originalParent = document.body;
+const myPocket = document.getElementById("pocket_container");
+const originalPositions = {};
 
 function moveStone(event) {
   const stone = event.target.closest(".stone");
-  if (stone.parentElement === movePocket) {
-    originalParent.appendChild(stone);
+  if (stone.parentElement === myPocket) {
+    //돌이 pocket에 있는 경우 원래 위치로 돌려놓기
+    const originalPosition = originalPositions[stone];
+    originalPosition.appendChild(stone);
+    stone.classList.remove("in-pocket"); //css에
   } else {
-    movePocket.appendChild(stone);
+    //돌을 pocket으로 이동
+    originalPositions[stone] = stone.parentElement; // 원래 위치 저장
+    myPocket.appendChild(stone);
+    stone.classList.add("in-pocket");
   }
 }
 
-function removeStone(event) {}
-// HTMLCollection을 배열로 변환 후 각 요소에 이벤트 리스너 추가
+// // HTMLCollection을 배열로 변환 후 각 요소에 이벤트 리스너 추가
 Array.from(myStones).forEach((stone) => {
   stone.addEventListener("click", moveStone);
 });
