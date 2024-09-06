3118850d7ddd841f5881a9b0fc03b3d96b3dec69
Munkt <110991832+Munkt@users.noreply.github.com>
2024-09-07 02:08:59 +0900

    0907

--- js/stone.js
+++ js/stone.js
@@ -1,23 +1,29 @@
-// const myStones = document.getElementsByClassName("stone");
-// const myPocket = document.getElementById("pocket_container");
-// const originalPositions = {};
+//작동 안함 https://github.com/geosigno/simpleParallax.js
+// import SimpleParallax from "simple-parallax-js/vanilla";
+// const image = document.getElementsByClassName("parallax_up");
+// new SimpleParallax(image);
 
-// function moveStone(event) {
-//   const stone = event.target.closest(".stone");
-//   if (stone.parentElement === myPocket) {
-//     //돌이 pocket에 있는 경우 원래 위치로 돌려놓기
-//     const originalPosition = originalPositions[stone];
-//     originalPosition.appendChild(stone);
-//     stone.classList.remove("in-pocket"); //css에
-//   } else {
-//     //돌을 pocket으로 이동
-//     originalPositions[stone] = stone.parentElement; // 원래 위치 저장
-//     myPocket.appendChild(stone);
-//     stone.classList.add("in-pocket");
-//   }
-// }
+const myStones = document.getElementsByClassName("stone");
+const myPocket = document.getElementById("pocket_container");
+const originalPositions = {};
 
-// // // HTMLCollection을 배열로 변환 후 각 요소에 이벤트 리스너 추가
-// Array.from(myStones).forEach((stone) => {
-//   stone.addEventListener("click", moveStone);
-// });
+function moveStone(event) {
+  const stone = event.target.closest(".stone");
+  if (stone.parentElement === myPocket) {
+    //돌이 pocket에 있는 경우 원래 위치로 돌려놓기
+    const originalPosition = originalPositions[stone];
+    originalPosition.appendChild(stone);
+    stone.classList.remove("in-pocket"); //css에
+  } else {
+    //돌을 pocket으로 이동
+    originalPositions[stone] = stone.parentElement; // 원래 위치 저장
+    myPocket.appendChild(stone);
+    stone.classList.add("in-pocket");
+    document;
+  }
+}
+
+// // HTMLCollection을 배열로 변환 후 각 요소에 이벤트 리스너 추가
+Array.from(myStones).forEach((stone) => {
+  stone.addEventListener("click", moveStone);
+});
